a6b742e6dc57ffff27cd225b65a81ae62cb15cef
Kotomi <piitun3232@gmail.com>
2022-05-01 16:50:33 +0900

    graph component

--- src/components/Graph.tsx
+++ src/components/Graph.tsx
@@ -1,2 +1,8 @@
+import React from "react"
 import Highcharts from 'highcharts';
 import HighchartsReact from 'highcharts-react-official';
+
+const Graph: React.FC<Props> = ({ populationdata}) => {
+  let series: Highcharts.SeriesOptionstype[] = []
+  let categories = []
+}
